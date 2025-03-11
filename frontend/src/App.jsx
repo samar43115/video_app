19eebfbb4fe190976e4a382675b72129c1965c92
explo65 <samarlamba65@gmail.com>
2025-03-11 18:54:51 +0530

    some change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/video/:videoUrl" element={<VideoPage />} />
+          <Route path="/video/:id" element={<VideoPage />} />
         </Routes>
       </div>
     </Router>
--- frontend/src/components/Videoplayer.jsx
+++ frontend/src/components/Videoplayer.jsx
@@ -3,21 +3,20 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios'
 
 export const Videoplayer = () => {
-  const { videoUrl } = useParams();
+  const { id } = useParams();
   const [video, setVideo] = useState(null)
  
 
   useEffect(() => {
     axios
-      .get(`https://video-app-2oeo.onrender.com/api/videos/${videoUrl}`)
+      .get(`https://video-app-2oeo.onrender.com/api/videos/${id}`)
       .then((response) => {
         setVideo(response.data)
         console.log(response.data)
       })
     .catch((error)=> console.log("Error fetching video:",error))
-  }, [videoUrl])
+  }, [id])
   
-  if(!video) return <div className='text-center text-white mt-10'>Loading.....</div>
   return (
     <div className="flex flex-col items-center mt-6 px-4">
       <h2 className="text-white text-2xl font-semibold">{video.title}</h2>
@@ -25,7 +24,9 @@ export const Videoplayer = () => {
         controls
         className="w-full max-w-3xl mt-4 rounded-lg shadow-lg"
       >
+        <source src={video.id} type="video/mp4" />
+        Your browser does not support the video tag.
       </video>
     </div>
-  )
+  );
 }
